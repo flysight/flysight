b4dcf628682791c1af512209fde900f3188606fc
Bionic Avionics <michael@flysight.ca>
2010-10-29 22:43:58 -0600

    deleted old files in Mac FlySightViewer

--- FlySightViewer/Mac/FSWindow.h
+++
@@ -1,9 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-@interface FSWindow : NSWindow
-{
-}
-
--(IBAction) print: (id) sender;
-
-@end
--- FlySightViewer/Mac/ToolTipTextField.h
+++
@@ -1,7 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-@interface ToolTipTextField : NSTextField
-{
-}
-
-@end
