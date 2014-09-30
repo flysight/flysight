d7375dbf6420dc0acd9eaf7c1051b58c33d1b971
Michael Cooper <crwper@gmail.com>
2014-09-29 21:04:21 -0600

    Improved description of configuration parameters

--- src/Config.c
+++ src/Config.c
@@ -121,9 +121,9 @@ TZ_Offset: 0     ; Timezone offset of output files in seconds\r\n\
 ;          level.\r\n\
 \r\n\
 Window:        0 ; Alarm window (m)\r\n\
-DZ_Elev:       0 ; Ground elevation (m)\r\n\
+DZ_Elev:       0 ; Ground elevation (m above sea level)\r\n\
 \r\n\
-Alarm_Elev: 1000 ; Alarm elevation (m)\r\n\
+Alarm_Elev: 1000 ; Alarm elevation (m above ground level)\r\n\
 Alarm_Type:    0 ; Alarm type\r\n\
                  ;   0 = No alarm\r\n\
                  ;   1 = Beep\r\n\
